5d56578ec76499bb93cde870b937242595b9d33a
UADER-PEC <31574049+lu7did@users.noreply.github.com>
2025-12-31 15:29:12 -0300

    testDDS

--- build/CMakeFiles/ADX-ddsPIO.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for ADX-ddsPIO.
-# This may be replaced when dependencies are built.
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/Users/PCOLLA/Documents/GitHub/ADX-ddsPIO/build/_deps/picotool/"
-};
