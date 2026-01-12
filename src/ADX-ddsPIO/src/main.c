537c0a4c9d03d59f19a78cdb005fd7d7497b9620
UADER-PEC <31574049+lu7did@users.noreply.github.com>
2026-01-11 22:38:39 -0300

    ADX-ddsPIO_1stOk

--- src/ADX-ddsPIO/src/main.c
+++ src/ADX-ddsPIO/src/main.c
@@ -1,5 +1,10 @@
 /*
  * =======================================================================================
+ * ADX-ddsPIO
+ * (c) Dr. Pedro E. Colla (LU7DZ) <pedro.colla@gmail.com>
+ * 
+ * new generation rp2040 ADX based digital transceiver 
+ * 
  * This is mainly an integration effort with some new code developed for this project
  *  
  * The integration effort is being built on top of previous work from many parties,
